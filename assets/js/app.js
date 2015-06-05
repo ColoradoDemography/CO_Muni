8ea799fac9f8f8d3c8e5d5b9fe7778a089086514
Daniel Trone <royhobbstn@yahoo.com>
2015-06-05 19:43:06 +0000

    update muni data references

--- assets/js/app.js
+++ assets/js/app.js
@@ -78,7 +78,7 @@ var counties = L.esri.featureLayer('http://services.arcgis.com/IamIM3RJ5xHykalK/
   }).addTo(map);; 
   
   
-var annexations = L.esri.featureLayer('http://services.arcgis.com/IamIM3RJ5xHykalK/arcgis/rest/services/Web_Annexations03252015/FeatureServer/0', {
+var annexations = L.esri.featureLayer('http://services.arcgis.com/IamIM3RJ5xHykalK/arcgis/rest/services/Web_Annexations06052015/FeatureServer/0', {
     simplifyFactor: featsimplify,
     style: function (feature) {
 		return getcitystyle(feature);
