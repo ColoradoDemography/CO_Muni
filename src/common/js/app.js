134043425b1d2b6ac6c20535ede9e83bd9580cfc
Daniel Trone <royhobbstn@yahoo.com>
2016-07-27 16:58:15 +0000

    https

--- src/common/js/app.js
+++ src/common/js/app.js
@@ -88,7 +88,7 @@ var counties = L.esri.featureLayer({
 //style handled through 'getcitystyle' function
 //popup handled through 'createPopup' function
 var annexations = L.esri.featureLayer({
-  url: 'http://services.arcgis.com/IamIM3RJ5xHykalK/arcgis/rest/services/Web_Annexations07272016/FeatureServer/0', 
+  url: 'https://services.arcgis.com/IamIM3RJ5xHykalK/arcgis/rest/services/Web_Annexations07272016/FeatureServer/0', 
     simplifyFactor: featsimplify,
     style: function (feature) {
 		return getcitystyle(feature);
