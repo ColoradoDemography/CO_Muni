42afbe67f24750776f86b1f8d0b781bf105b4b5f
Daniel Trone <royhobbstn@users.noreply.github.com>
2016-07-28 09:57:55 -0600

    Update app.js
    county layer

--- src/common/js/app.js
+++ src/common/js/app.js
@@ -68,7 +68,7 @@ map = L.map("map", {
 });
 
 var counties = L.esri.featureLayer({
-    url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0',
+    url: 'http://services.arcgis.com/IamIM3RJ5xHykalK/arcgis/rest/services/Webmap_County/FeatureServer/0',
       simplifyFactor: 0.5,
     style: function (feature) {
         return {color: 'black', weight: countyweight };
